8a1684b4c67bba28f96796b8627c7d55f5f06fd8
Nowaaru <nowaaru@proton.me>
2024-03-24 22:13:26 -0700

    commands guhhhh

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use clap::{Parser, Subcommand};
 use std::path::PathBuf;
 
-mod provider;
-mod tests;
-mod nexus;
-mod query;
 mod r#mod;
+mod nexus;
+mod nix;
+mod provider;
+mod query;
+mod tests;
 
 // https://docs.rs/clap/latest/clap/_derive/index.html#arg-attributes
 // https://docs.rs/clap/latest/clap/_derive/_tutorial/chapter_0/index.html
@@ -22,31 +23,70 @@ struct Cli {
     debug: u8,
 
     #[command(subcommand)]
-    command: Option<Commands>,
+    command: Commands,
 }
 
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    // The name of the provider.
-    #[arg(short, long)]
-    provider: i32,
-}
+#[derive(Debug, Subcommand)]
+enum Provider {
+    Nexus {
+        #[arg(value_name = "GAME-DOMAIN-NAME")]
+        domain: String,
+        #[arg(value_name = "MOD-ID")]
+        mod_id: i32,
+        #[arg(value_name = "FILE-ID")]
+        file_id: i32,
 
-#[derive(Subcommand)]
-enum Commands {
-    Fetch {
-        provider: Option<String>,
-        mod_id: Option<String>,
-        file_id: Option<String>,
-        expire: Option<String>,
+        #[arg(short, long)]
+        expire: Option<usize>,
+        #[arg(short, long)]
         key: Option<String>,
     },
+}
 
-    Init
+#[derive(Debug, Subcommand)]
+enum Commands {
+    #[command(arg_required_else_help = true)]
+    Fetch {
+        #[command(subcommand)]
+        provider: Provider,
+    },
+
+    Checkout {
+        provider: Option<String>,
+    },
+
+    #[command(arg_required_else_help = true)]
+    Init {
+        r#where: Option<String>,
+    },
 }
 
 #[tokio::main]
 async fn main() {
+    let args = Cli::parse();
+    match args.command {
+        Commands::Fetch { provider } => match provider {
+            Provider::Nexus {
+                domain,
+                mod_id,
+                file_id,
+                expire,
+                key,
+            } => {}
+        },
+        Commands::Init { r#where } => match r#where {
+            Some(here) => (),
+            None => (),
+        },
 
+        // iterates through the lockfile and
+        // generates download links (if necessary),
+        // lockfile members should probably have `isApi`
+        // to indicate whether nix-mod-manager should use
+        // nmm-cli or just run straight-up fetchurl. :thinking: /shrug
+        Commands::Checkout { provider } => match provider {
+            Some(string) => (),
+            None => (),
+        },
+    };
 }
