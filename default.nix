136c2ff2c80628d0f295777735ddfc133c9a3d44
Nowaaru <nowaaru@proton.me>
2024-03-29 15:04:25 -0700

    go for prebuilt solution maybe?
    hmmmmmmm

--- default.nix
+++ default.nix
@@ -7,27 +7,38 @@
   cargo,
   pkg-config,
   openssl,
-
+  stdenv
 }:
-rustPlatform.buildRustPackage rec {
-  pname = "nmm-cli";
+stdenv.mkDerivation {
+  name = "nmm-cli";
   version = "0.0.1";
 
-  src = lib.cleanSource ./.;
+  src = ./target/release/nmm-cli;
 
-  nativeBuildInputs = [ rustc cargo openssl.dev pkg-config ];
-
-  cargoLock.lockFile = ./Cargo.lock;
-
-  buildPhase = ''
-    export HOME=$(pwd)
+  installPhase = ''
+    cp $src $out
   '';
-
-  meta = with lib; {
-    description = "A fast line-oriented regex search tool, similar to ag and ack";
-    homepage = "https://github.com/BurntSushi/ripgrep";
-    license = licenses.unlicense;
-    maintainers = [];
-  };
 }
 
+# rustPlatform.buildRustPackage rec {
+#   pname = "nmm-cli";
+#   version = "0.0.1";
+#
+#   src = lib.cleanSource ./.;
+#
+#   nativeBuildInputs = [ rustc cargo openssl.dev pkg-config ];
+#
+#   cargoLock.lockFile = ./Cargo.lock;
+#
+#   buildPhase = ''
+#     export HOME=$(pwd)
+#   '';
+#
+#   meta = with lib; {
+#     description = "A fast line-oriented regex search tool, similar to ag and ack";
+#     homepage = "https://github.com/BurntSushi/ripgrep";
+#     license = licenses.unlicense;
+#     maintainers = [];
+#   };
+# }
+#
